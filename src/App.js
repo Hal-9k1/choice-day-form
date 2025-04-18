606775b69874742ce764705ff1bba472498615ea
Eddy Meals <edimea211@gmail.com>
2025-04-18 08:27:23 -0700

    Style stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
+import './App.css';
 
 const MIME = 'application/x-choice-day-selection';
 const PERIODS = 6;
@@ -6,23 +7,29 @@ const CHOICE_DATA = [
   {
     name: 'class 1',
     desc: 'desc 1',
+    textColor: 'white',
+    backgroundColor: 'red',
     periods: [1],
   },
   {
     name: 'class 2',
     desc: 'desc 2',
+    textColor: 'white',
+    backgroundColor: 'blue',
     periods: [1],
   },
   {
     name: 'class 3',
     desc: 'desc 3',
+    textColor: 'white',
+    backgroundColor: 'green',
     periods: [2],
   },
 ];
 
 function getDefaultChoices() {
   return Array(PERIODS).fill(null).map((_, i) =>
-    CHOICE_DATA.filter(choice => choice.periods.includes(i))
+    CHOICE_DATA.filter(choice => choice.periods.includes(i + 1))
   );
 }
 
@@ -44,8 +51,20 @@ function getOrdinal(num) {
 
 export default function App() {
   const [choices, setChoices] = useState(getDefaultChoices);
-  const [selected, setSelected] = useState(1);
+  const [selected, setSelected] = useState(0);
   const [transferring, setTransferring] = useState(null);
+  const [dropTarget, setDropTarget] = useState(null);
+  const [dropRefCount, setDropRefCount] = useState(0);
+  const onDragEnd = useCallback(() => {
+    setTransferring(null);
+    setDropTarget(null);
+    setDropRefCount(0);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('dragend', onDragEnd);
+    return () => window.removeEventListener('dragend', onDragEnd);
+  }, []);
 
   const onDragStart = (i, e) => {
     e.dataTransfer.setData(MIME, i);
@@ -58,9 +77,24 @@ export default function App() {
     e.dataTransfer.dropEffect = 'move';
   };
 
+  const onDragEnter = (i, e) => {
+    if (e.dataTransfer.types.includes(MIME) && i !== transferring) {
+      setDropTarget(i);
+      setDropRefCount(n => n + 1);
+    }
+  };
+
+  const onDragLeave = (i, e) => {
+    if (e.dataTransfer.types.includes(MIME) && i !== transferring) {
+      if (dropRefCount === 1) {
+        setDropTarget(null);
+      }
+      setDropRefCount(n => n - 1);
+    }
+  };
+
   const onDrop = (i, e) => {
     e.preventDefault();
-    console.log(choices[selected]);
     setChoices(choices.with(selected, choices[selected]
       .with(i, choices[selected][e.dataTransfer.getData(MIME)])
       .with(e.dataTransfer.getData(MIME), choices[selected][i])
@@ -69,12 +103,9 @@ export default function App() {
   return (
     <div className='App'>
       <div className='App-intro'>
+        <h1>ASTI Choice Day 2025 class selection</h1>
         <p>Rank your class selection for Choice Day!</p>
         <ul>
-          <li>
-            You will only be enrolled into a class one time, even if it&apos;s offered for multiple
-            periods.
-          </li>
           <li>
             Click on each period, then reorder the class choices so your more preferred classes for
             that period are on top.
@@ -84,6 +115,10 @@ export default function App() {
           </li>
           <li>
             We will try to give everyone at least one of their top choices.
+          </li>
+          <li>
+            You will only be enrolled into a class one time, even if it&apos;s offered for multiple
+            periods.
           </li>
           <li>
             <strong>No transfers. Please do not ask.</strong>
@@ -99,7 +134,7 @@ export default function App() {
                 + (i % 2 ? ' App-table-row-alt' : '')
                 + (i === selected ? ' App-table-row-selected' : '')
               }
-              onClick={() => setSelected(i + 1)}
+              onClick={() => setSelected(i)}
               key={`key-${i}`}
             >
               {i + 1}
@@ -111,11 +146,15 @@ export default function App() {
           <div className='App-ranking-body'>
             {choices[selected].map((choice, i) => (
               <div
-                className={
-                  `App-ranking-tile${transferring === i ? ' App-ranking-transferring' : ''}`
+                className={'App-ranking-tile'
+                  + (transferring === i ? ' App-ranking-transferring' : '')
+                  + (dropTarget === i ? ' App-ranking-drop-target' : '')
                 }
+                onDragEnter={e => onDragEnter(i, e)}
+                onDragLeave={e => onDragLeave(i, e)}
                 onDragOver={e => onDragOver(i, e)}
                 onDrop={e => onDrop(i, e)}
+                style={{ color: choice.textColor, backgroundColor: choice.backgroundColor }}
                 key={`key-${i}`}
               >
                 <div
